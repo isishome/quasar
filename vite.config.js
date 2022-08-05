fc4207c10b41665c29f5426ee19842dc0a02189c
isishome <isishome@hanmail.net>
2022-08-06 01:52:28 +0900

    update build script

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,10 @@ export default defineConfig(({ command }) => {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url))
       }
+    },
+    build: {
+      outDir: './quasar',
+      emptyOutDir: true
     }
   }
 })
